68520a82c51ab4498c1b2b91c8421e4baad9fba2
peterjohnhunt <git@peterjohnhunt.com>
2019-05-23 00:13:57 -0500

    Use WP CLI Version Ticket

--- command.php
+++ command.php
@@ -10,7 +10,7 @@ class Publish_Command {
 
     protected $theme;
 
-    protected $version;
+    protected $new_version;
 
     protected $theme_dir;
 
@@ -46,60 +46,32 @@ class Publish_Command {
         $command = 'site';
         $theme   = wp_get_theme();
 
-        $this->theme       = $theme;
-        $this->version     = $theme->version;
-        $this->theme_dir   = $theme->get_stylesheet_directory();
-        $this->dryrun      = WP_CLI\Utils\get_flag_value($assoc_args, 'dryrun');
-        $this->message     = WP_CLI\Utils\get_flag_value($assoc_args, 'message');
-        $this->repository  = WP_CLI\Utils\get_flag_value($assoc_args, 'repository');
+        $this->theme           = $theme;
+        $this->current_version = $theme->version;
+        $this->theme_dir       = $theme->get_stylesheet_directory();
+        $this->dryrun          = WP_CLI\Utils\get_flag_value($assoc_args, 'dryrun');
+        $this->message         = WP_CLI\Utils\get_flag_value($assoc_args, 'message');
+        $this->repository      = WP_CLI\Utils\get_flag_value($assoc_args, 'repository');
 
         if ( count($args) > 1 ) {
-            list( $command, $version ) = $args;
+            list( $command, $new_version ) = $args;
         } else {
-            list( $version ) = $args;
+            list( $new_version ) = $args;
         }
 
-        if ( in_array($version, self::SEMVERSIONS) ) {
-            $version = $this->_sem_version( $version );
-        }
+        $new_version = WP_CLI\Utils\increment_version($this->current_version, $new_version);
 
-        if ( !preg_match('/^'.self::VERSIONREG.'/', $version) ) WP_CLI::error('incorrect version command');
+        if ( !preg_match('/^'.self::VERSIONREG.'/', $new_version) ) WP_CLI::error('incorrect version command');
 
-        if ( version_compare($this->version,$version) !== -1 ) WP_CLI::error("New version number: {$version} is below current version: {$this->version}");
+        if ( version_compare($this->current_version,$new_version) !== -1 ) WP_CLI::error("New version number: {$new_version} is below current version: {$this->current_version}");
 
         if ( !method_exists( $this, $command ) ) WP_CLI::error("{$command} command not found");
 
-        $updated = $this->$command($version);
+        $updated = $this->$command($new_version);
 
         if ( !$updated ) WP_CLI::error('unable to publish version');
 
-        WP_CLI::success( "{$command} updated from {$this->version} to {$version}" );
-    }
-
-    protected function _sem_version($version){
-        $pieces = explode('.', $this->version);
-
-        if (count($pieces) != 3) WP_CLI::error('Unable to parse theme version');
-
-        list($major, $minor, $patch) = $pieces;
-
-        if ( in_array($version, array('major', 'minor')) ){
-            $patch = '0';
-        }
-
-        if ( $version == 'major' ){
-            $minor = '0';
-        }
-        
-        if ($version == 'major'){
-            $major++;
-        } elseif ($version == 'minor'){
-            $minor++;
-        } elseif ($version == 'patch'){
-            $patch++;
-        }
-
-        return implode('.', array($major, $minor, $patch));
+        WP_CLI::success( "{$command} updated from {$this->current_version} to {$new_version}" );
     }
 
     protected function _update_file($path, $find, $replace, $count=1){
@@ -118,30 +90,30 @@ class Publish_Command {
         return ($saved !== false);
     }
 
-    function site($version){
+    function site($new_version){
         if ( !$this->dryrun ){
             $this->dryrun = true;
-            $verified = $this->site($version);
+            $verified = $this->site($new_version);
             if ( !$verified ) WP_CLI::error('Unable to complete the release');
             $this->dryrun = false;
         }
 
-        $style        = $this->style($version);
-        $changelog    = $this->changelog($version);
+        $style        = $this->style($new_version);
+        $changelog    = $this->changelog($new_version);
 
         return ($style && $changelog);
     }
 
-    function style($version){
+    function style($new_version){
         $path     = trailingslashit($this->theme_dir) . 'style.css';
-        $find     = '/^(\s*Version:\s?)'.$this->version.'$/m';
-        $replace  = '${1}'.$version;
+        $find     = '/^(\s*Version:\s?)'.$this->current_version.'$/m';
+        $replace  = '${1}'.$new_version;
         return $this->_update_file($path, $find, $replace);
     }
 
-    function changelog($version){
+    function changelog($new_version){
         if ( !$this->message ) {
-            $message = WP_CLI\Utils\launch_editor_for_input("\n\n# Please enter the release notes.\n# Lines starting with '#' will be ignored and an empty message aborts the release.\n# current version: v{$this->version}\n# releasing version: v{$version}");
+            $message = WP_CLI\Utils\launch_editor_for_input("\n\n# Please enter the release notes.\n# Lines starting with '#' will be ignored and an empty message aborts the release.\n# current version: v{$this->current_version}\n# releasing version: v{$new_version}");
             $message = preg_replace('/^#[^#].*$\s+/m', '', $message);
             $this->message = trim($message);
         }
@@ -151,8 +123,8 @@ class Publish_Command {
         $date    = current_time('Y-m-d');
         $folder  = is_multisite() ? $this->theme_dir : ABSPATH;
         $path    = trailingslashit( $folder ) . 'changelog.md';
-        $find    = '/^(## \[v'.$this->version.'\])/m';
-        $link    = $this->repository ? "[v{$version}]($this->repository/compare/v{$this->version}...v{$version})" : "v{$version}";
+        $find    = '/^(## \[v'.$this->current_version.'\])/m';
+        $link    = $this->repository ? "[v{$new_version}]($this->repository/compare/v{$this->current_version}...v{$new_version})" : "v{$new_version}";
         $replace = "## {$link} {$date}\n{$this->message}\n\n\\1";
         return $this->_update_file($path, $find, $replace);
     }
