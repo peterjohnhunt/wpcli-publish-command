efe8bbe438df8347fdbf0aee4eae9a704dab2c2c
peterjohnhunt <git@peterjohnhunt.com>
2019-05-22 23:45:24 -0500

    Readme and Docblocks

--- command.php
+++ command.php
@@ -17,7 +17,31 @@ class Publish_Command {
     protected $repository;
 
     protected $message;
-
+    
+    /**
+     * Publish version of site and theme
+     * 
+     * ## OPTIONS
+     * <version>...
+     * : Version or subcommand followed by version number
+     * [--dryrun=<dryrun>]
+     * : whether to actually update files, or run a dry run with not actual file changes.
+     * ---
+     * default: false
+     * options:
+     *    - true
+     *    - false
+     * ---
+     * [--message=<message>]
+     * : release notes if full editor is not needed
+     * [--repo=<repo>]
+     * : Git repo url for changelog compare link
+     * 
+     * ## EXAMPLES
+     *    wp publish major|minor|patch|1.0.0
+     *    wp publish style major|minor|patch|1.0.0
+     *    wp publish changelog major|minor|patch|1.0.0
+     */
     function __invoke( $args, $assoc_args ) {
         $command = 'site';
         $theme   = wp_get_theme();
