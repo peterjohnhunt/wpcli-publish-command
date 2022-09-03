5880ff27b2d3ef9807b0304b60c5415c498a8452
peterjohnhunt <git@peterjohnhunt.com>
2022-09-02 20:50:21 -0500

    Overhaul to work for plugins; mu-plugins; themes; composer

--- command.php
+++ command.php
@@ -7,16 +7,6 @@ class Publish_Command {
     const SEMVERSIONS = array('patch', 'minor', 'major');
 
     const VERSIONREG  = '[0-9]+\.[0-9]+\.[0-9]+';
-
-    protected $theme;
-
-    protected $new_version;
-
-    protected $theme_dir;
-
-    protected $repository;
-
-    protected $message;
     
     /**
      * Publish version of site and theme
@@ -39,94 +29,255 @@ class Publish_Command {
      * 
      * ## EXAMPLES
      *    wp publish major|minor|patch|1.0.0
-     *    wp publish style major|minor|patch|1.0.0
-     *    wp publish changelog major|minor|patch|1.0.0
+     *    wo publish site major|minor|patch|1.0.0
+     *    wo publish theme <theme> major|minor|patch|1.0.0
+     *    wo publish plugin <plugin> major|minor|patch|1.0.0
      */
     function __invoke( $args, $assoc_args ) {
-        $command = 'site';
-        $theme   = wp_get_theme();
+        $this->command = count($args) == 1 ? '' : $args[0];
+        $this->folder  = count($args) != 3 ? '' : $args[1];
+        $this->version = end($args);
 
-        $this->theme           = $theme;
-        $this->current_version = $theme->version;
-        $this->theme_dir       = $theme->get_stylesheet_directory();
-        $this->dryrun          = WP_CLI\Utils\get_flag_value($assoc_args, 'dryrun');
-        $this->message         = WP_CLI\Utils\get_flag_value($assoc_args, 'message');
-        $this->repository      = WP_CLI\Utils\get_flag_value($assoc_args, 'repository');
+        $this->message    = $assoc_args['message'] ?? '';
+        $this->repository = $assoc_args['repository'] ?? '';
 
-        if ( count($args) > 1 ) {
-            list( $command, $new_version ) = $args;
-        } else {
-            list( $new_version ) = $args;
-        }
+        if ( !$this->command ) $this->command = $this->get_command();
 
-        $new_version = WP_CLI\Utils\increment_version($this->current_version, $new_version);
+        $method = 'publish_'.$this->command;
+        
+        if ( !method_exists( $this, $method ) ) WP_CLI::error("{$this->command} command not found");
+        
+        $updated = $this->$method();
 
-        if ( !preg_match('/^'.self::VERSIONREG.'/', $new_version) ) WP_CLI::error('incorrect version command');
+        if ( !$updated ) WP_CLI::error('Unable to publish version');
 
-        if ( version_compare($this->current_version,$new_version) !== -1 ) WP_CLI::error("New version number: {$new_version} is below current version: {$this->current_version}");
-
-        if ( !method_exists( $this, $command ) ) WP_CLI::error("{$command} command not found");
-
-        $updated = $this->$command($new_version);
-
-        if ( !$updated ) WP_CLI::error('unable to publish version');
-
-        WP_CLI::success( "{$command} updated from {$this->current_version} to {$new_version}" );
+        WP_CLI::success( $updated );
     }
 
-    protected function _update_file($path, $find, $replace, $count=1){
-        if ( !file_exists($path) ) return WP_CLI::line( "{$path} doesn't exist or is empty" );
+    function get_command() {
+        $plugin = $this->get_plugin($this->folder);
+        if ( $plugin ) return 'plugin';
+
+        $theme = $this->get_theme($this->folder);
+        if ( $theme ) return 'theme';
+
+        return 'site';
+    }
+
+    function get_message($message, $current, $new) {
+        if ($this->message) return $this->message;
+
+        $message = WP_CLI\Utils\launch_editor_for_input("\n\n# Please enter the release notes.\n# Lines starting with '#' will be ignored and an empty message aborts the release.\n# Updating {$this->command}: {$message}\n# Current version: v{$current}\n# Releasing version: v{$new}");
+        $message = preg_replace('/^#[^#].*$\s+/m', '', $message);
+        $message = trim($message);
+
+        $this->message = $message;
+    }
+
+    function version_check($current, $new) {
+        if ( !preg_match('/^'.self::VERSIONREG.'/', $new) ) WP_CLI::error('invalid version option');
+
+        if ( version_compare($current, $new) !== -1 ) WP_CLI::error("New version number: {$new} is below current version: {$current}");
+    }
+
+    function parse_theme_folder() {
+        if (strpos(get_theme_root(), getcwd()) === false) return basename(getcwd());
+
+        $relative = str_replace(trailingslashit(get_theme_root()), '', getcwd());
+        $pieces   = explode('/', $relative);
+        return $pieces[0] ?? false;
+    }
+
+    function get_theme($folder=false) {
+        if ( !$folder ) $folder = $this->parse_theme_folder();
+
+        $fetcher = new WP_CLI\Fetchers\Theme();
+
+        return $fetcher->get( $folder );
+    }
+
+    function get_theme_version($folder=false) {
+        $theme = $this->get_theme($folder);
+
+        return $theme ? $theme->get('Version') : false;
+    }
+
+    function get_theme_folder($folder=false) {
+        $theme = $this->get_theme($folder);
+
+        return $theme ? $theme->get_stylesheet_directory() : false;
+    }
+
+    function parse_plugin_folder() {
+        if (strpos(WP_PLUGIN_DIR, getcwd()) === false) return basename(getcwd());
+
+        $relative = str_replace(trailingslashit(WP_PLUGIN_DIR), '', getcwd());
+        $pieces   = explode('/', $relative);
+        return $pieces[0] ?? false;
+    }
+
+    function get_plugin($folder=false) {
+        if ( !$folder ) $folder = $this->parse_plugin_folder();
+
+        $fetcher = new WP_CLI\Fetchers\Plugin();
+        $plugin  = $fetcher->get( $folder );
+
+        if ( !$plugin ) return $this->get_mu_plugin($folder);
+
+        $data = get_plugin_data(trailingslashit(WP_PLUGIN_DIR) . $plugin->file, false);
+
+        return array_merge($data, [
+            'PluginFile'   => trailingslashit(WP_PLUGIN_DIR) . $plugin->file,
+            'PluginFolder' => trailingslashit(WP_PLUGIN_DIR) . $folder
+        ]);
+    }
+
+    function get_mu_plugin_data($file) {
+        $default_headers = array(
+            'Name'        => 'Plugin Name',
+            'PluginURI'   => 'Plugin URI',
+            'Version'     => 'Version',
+            'Description' => 'Description',
+            'Author'      => 'Author',
+            'AuthorURI'   => 'Author URI',
+            'TextDomain'  => 'Text Domain',
+            'DomainPath'  => 'Domain Path',
+            'Network'     => 'Network',
+            'RequiresWP'  => 'Requires at least',
+            'RequiresPHP' => 'Requires PHP',
+            'UpdateURI'   => 'Update URI',
+        );
+     
+        return get_file_data( $file, $default_headers, 'plugin' );
+    }
+
+    function get_mu_plugin($folder=false) {
+        if ( !$folder ) $folder = $this->parse_plugin_folder();
+
+        if ( !$folder ) return;
+
+        foreach ( get_mu_plugins() as $file => $plugin ) {
+            if ( file_exists(trailingslashit(WPMU_PLUGIN_DIR) . trailingslashit($folder) . $file) ) {
+                $data = $this->get_mu_plugin_data(trailingslashit(WPMU_PLUGIN_DIR) . trailingslashit($folder) . $file);
+                return array_merge($data, [
+                    'PluginFile'   => trailingslashit(WPMU_PLUGIN_DIR) . trailingslashit($folder) . $file,
+                    'PluginFolder' => trailingslashit(WPMU_PLUGIN_DIR) . trailingslashit($folder)
+                ]);
+            }
+        }
+    }
+
+    function get_plugin_version($folder=false) {
+        $plugin = $this->get_plugin($folder);
+
+        return $plugin['Version'] ?? false;
+    }
+
+    function get_plugin_folder($folder=false) {
+        $plugin = $this->get_plugin($folder);
+
+        return $plugin['PluginFolder'] ?? false;
+    }
+
+    function get_plugin_file($folder=false) {
+        $plugin = $this->get_plugin($folder);
+
+        return $plugin['PluginFile'] ?? false;
+    }
+
+    protected function update_file($path, $find, $replace, $count=1){
+        if ( !file_exists($path) ) return WP_CLI::line( "{$path} doesn't exist" );
 
         $contents = file_get_contents($path);
 
-        if (!$contents) return WP_CLI::line( "{$path} doesn't exist or is empty" );
+        if (!$contents) return WP_CLI::line( "{$path} is empty" );
 
-        $updated  = preg_replace($find, $replace, $contents, $count);
+        $updated = preg_replace($find, $replace, $contents, $count);
 
         if (!$updated || $contents == $updated) return WP_CLI::line( "{$path} failed or wasn't changed" );
 
-        $saved = !$this->dryrun ? file_put_contents($path, $updated) : true;
+        $saved = file_put_contents($path, $updated);
         
         return ($saved !== false);
     }
 
-    function site($new_version){
-        if ( !$this->dryrun ){
-            $this->dryrun = true;
-            $verified = $this->site($new_version);
-            if ( !$verified ) WP_CLI::error('Unable to complete the release');
-            $this->dryrun = false;
-        }
-
-        $style        = $this->style($new_version);
-        $changelog    = $this->changelog($new_version);
-
-        return ($style && $changelog);
+    function update_style($folder, $current, $new){
+        $path     = trailingslashit($folder) . 'style.css';
+        $find     = '/^(\s*Version:\s?)'.$current.'$/m';
+        $replace  = '${1}'.$new;
+        return $this->update_file($path, $find, $replace);
     }
 
-    function style($new_version){
-        $path     = trailingslashit($this->theme_dir) . 'style.css';
-        $find     = '/^(\s*Version:\s?)'.$this->current_version.'$/m';
-        $replace  = '${1}'.$new_version;
-        return $this->_update_file($path, $find, $replace);
-    }
-
-    function changelog($new_version){
-        if ( !$this->message ) {
-            $message = WP_CLI\Utils\launch_editor_for_input("\n\n# Please enter the release notes.\n# Lines starting with '#' will be ignored and an empty message aborts the release.\n# current version: v{$this->current_version}\n# releasing version: v{$new_version}");
-            $message = preg_replace('/^#[^#].*$\s+/m', '', $message);
-            $this->message = trim($message);
-        }
+    function update_changelog($folder, $current, $new){
+        if ( !$this->message ) $this->get_message(basename($folder), $current, $new);
 
         if ( !$this->message ) WP_CLI::error('Aborting release due to empty release notes');
 
         $date    = current_time('Y-m-d');
-        $folder  = is_multisite() ? $this->theme_dir : ABSPATH;
         $path    = trailingslashit( $folder ) . 'changelog.md';
-        $find    = '/^(## \[?v'.$this->current_version.'\]?)(\([^\)]\))?/m';
-        $link    = $this->repository ? "[v{$new_version}]($this->repository/compare/v{$this->current_version}...v{$new_version})" : "v{$new_version}";
-        $replace = "## {$link} {$date}\n{$this->message}\n\n\\1\\2";
-        return $this->_update_file($path, $find, $replace);
+        $find    = '/^(## \[?v'.$current.'\]?((\()(.+\/)([^)]+)(\)))?)/m';
+        $link    = $this->repository ? "" : "v{$new}";
+        $replace = "## [v{$new}]\\3\\4v{$current}...v{$new}\\6 {$date}\n{$this->message}\n\n\\1";
+        return $this->update_file($path, $find, $replace);
+    }
+
+    function update_composer($folder, $current, $new){
+        $path    = trailingslashit( $folder ) . 'composer.json';
+        $find     = '/^(\s*"version": ")'.$current.'(",?)$/m';
+        $replace  = '${1}'.$new.'${2}';
+        return $this->update_file($path, $find, $replace);
+    }
+
+    function update_plugin($path, $current, $new) {
+        $find     = '/^(\s.*Version[:\'],?\s?\'?)'.$current.'(\'\);)?$/mi';
+        $replace  = '${1}'.$new.'${2}';
+        $this->update_file($path, $find, $replace, 2);
+    }
+
+    function publish_plugin() {
+        $file    = $this->get_plugin_file($this->folder);
+        $root    = $this->get_plugin_folder($this->folder);
+        $current = $this->get_plugin_version($this->folder);
+        $new     = WP_CLI\Utils\increment_version($current, $this->version);
+        
+        $this->version_check($current, $new);
+
+        $this->update_changelog($root, $current, $new);
+        $this->update_composer($root, $current, $new);
+        $this->update_plugin($file, $current, $new);
+
+        return "Plugin updated from {$current} to {$new}";
+    }
+
+    function publish_theme() {
+        $root    = $this->get_theme_folder($this->folder);
+        $current = $this->get_theme_version($this->folder);
+        $new     = WP_CLI\Utils\increment_version($current, $this->version);
+
+        $this->version_check($current, $new);
+
+        $changelog = $this->update_changelog($root, $current, $new);
+        $style     = $this->update_style($root, $current, $new);
+        $composer  = $this->update_composer($root, $current, $new);
+
+        if ( !$changelog && !$style && !$composer ) return WP_CLI::error('Release failed');
+
+        return "Theme updated from {$current} to {$new}";
+    }
+
+    function publish_site() {
+        $theme   = wp_get_theme();
+        $current = $theme->get('Version');
+        $new     = WP_CLI\Utils\increment_version($current, $this->version);
+
+        $this->version_check($current, $new);
+
+        $changelog = $this->update_changelog(ABSPATH, $current, $new);
+        $style     = $this->update_style($theme->get_stylesheet_directory(), $current, $new);
+
+        if ( !$changelog && !$style ) return WP_CLI::error('Release failed');
+
+        return "Site updated from {$current} to {$new}";
     }
 }
 
